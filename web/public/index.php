dbd71084fdd3c28a4ff4e3e71c183ed4a9d9ca25
weary_adventurer <alex@browserling.com>
2019-11-29 00:34:43 +0300

    web: added error logging, added runtime division error

--- web/public/index.php
+++ web/public/index.php
@@ -1,5 +1,12 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__.'/../logs/php-error.log');
+ini_set('log_errors_max_len', 0);
+
+1 / 0;
+
 require_once "../private/library.php";
 
 say_hello();

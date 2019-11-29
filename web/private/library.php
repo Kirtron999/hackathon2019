a3d9af51ff6113a5e7bf3cf80163313a14bd62fb
weary_adventurer <alex@browserling.com>
2019-11-29 20:41:22 +0300

    web: added smarty template engine, displaying basic template

--- web/private/library.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function say_hello() {
-    echo "Hello from ../private/library.php!";
-}
-
-?>
--- web/public/index.php
+++ web/public/index.php
@@ -1,15 +1,21 @@
 <?php
 
+# Show all errors, log errors to ./php-errors.log
 error_reporting(E_ALL);
-ini_set('log_errors', 1);
-ini_set('error_log', __DIR__.'/../logs/php-error.log');
-ini_set('log_errors_max_len', 0);
+ini_set("log_errors", 1);
+ini_set("error_log", __DIR__ . "/php-errors.log");
+ini_set("log_errors_max_len", 0);
+ini_set("display_startup_errors", 1);
+ini_set("display_errors", 1);
 
-1 / 0;
+# Require Smarty template engine
+require_once "../private/deps/smarty-3.1.13/Smarty.class.php";
+$smarty = new Smarty();
+$smarty->setTemplateDir("../private/templates");
+$smarty->setCompileDir("../private/templates_c");
 
-require_once "../private/library.php";
-
-say_hello();
-phpinfo();
+# Load a sample template
+$smarty->assign("greeting", "world");
+$smarty->display("index.tmpl.html");
 
 ?>

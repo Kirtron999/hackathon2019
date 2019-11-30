bcceac41da380ac6d56fcfb2cb7f929493c265bb
weary_adventurer <alex@browserling.com>
2019-11-30 16:23:03 +0300

    web: nested categories

--- web/private/libs/get-categories.php
+++ web/private/libs/get-categories.php
@@ -12,7 +12,42 @@ function get_categories() {
                    GROUP BY categories.id;");
 
     $categories = $result->fetch_all(MYSQLI_ASSOC);
-    return $categories;
+
+    $results = array_filter($categories, function($value) {
+        return $value["parent_id"] == null;
+    });
+
+    $results = array_map(function($value) {
+        return [
+            "id" => $value["id"],
+            "name" => $value["name"],
+            "count" => $value["posts_count"],
+            "children" => []
+        ];
+    }, $results);
+
+    foreach ($categories as $category) {
+        if ($category["parent_id"] != null) {
+
+            $parent = null;
+            $key = null;
+            foreach ($results as $k => $result) {
+                if ($result["id"] == $category["parent_id"]) {
+                    $parent = $result;
+                    $key = $k;
+                    break;
+                }
+            }
+
+            $results[$key]["children"][] = [
+                "id" => $category["id"],
+                "name" => $category["name"],
+                "count" => $category["posts_count"]
+            ];
+        }    
+    }
+
+    return $results;
 }
 
 ?>

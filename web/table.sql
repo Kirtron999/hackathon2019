03286608e04901162dc3ea48c4bd2909e4fe659c
aipol <bit.kmail@gmail.com>
2019-11-30 12:35:13 +0300

    web: adding replies table

--- web/table.sql
+++ web/table.sql
@@ -22,8 +22,20 @@ CREATE TABLE `posts` (
     `title` varchar(255) NOT NULL collate utf8_bin,
 	`description` text collate utf8_bin,
 	`category` int(11) NOT NULL,
-    `price` numeric(18, 2) NOT NULL CHECK (`price` >= 0),
+    `price` int(11) NOT NULL CHECK (`price` >= 0),
     `is_task` BOOL NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+#replies
+DROP TABLE IF EXISTS `replies`;
+CREATE TABLE `replies` (
+	`id` int(11) NOT NULL auto_increment,
+    `post_id` int(11) NOT NULL,
+    FOREIGN KEY (`post_id`) REFERENCES `posts`(`id`),
+    `user_id` int(11) NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
+    `message` text collate utf8_bin,
 	PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
